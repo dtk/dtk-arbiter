a5efee007a4246b9b6d4740dc474bf3ef334bbaa
Ubuntu <ubuntu@ip-10-235-90-253.ec2.internal>
2015-11-03 14:02:26 +0000

    add option parsing, add init script

--- start.rb
+++ start.rb
@@ -5,8 +5,17 @@ Bundler.setup
 require 'eventmachine'
 require 'dotenv'
 require 'daemons'
+require 'optparse'
+
 require File.expand_path('lib/listener', File.dirname(__FILE__))
 require File.expand_path('lib/utils/facts', File.dirname(__FILE__))
+
+options = {}
+OptionParser.new do |opts|
+  opts.on("-p", "--pid", "Daemon PID file") do |pid|
+    options[:pid] = pid
+  end
+end.parse!
 
 # load environment configuration
 Dotenv.load
@@ -19,8 +28,8 @@ end
 Arbiter::PBUILDER_ID = facts['pbuilderid']
 
 
-Daemons.daemonize()
-File.open('/var/run/dtk-arbiter.pid', 'w') { |f| f.puts(Process.pid) }
+Daemons.daemonize(app_name: 'dtk-arbiter')
+File.open(options[:pid] || '/var/run/dtk-arbiter.pid', 'w') { |f| f.puts(Process.pid) }
 
 EM.run {
   Signal.trap('INT') { stop }
