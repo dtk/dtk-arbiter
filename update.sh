6a11bfb84ac5e6ab46dfae37dd1b361259c8a454
dduvnjak <dario.duvnjak@gmail.com>
2015-11-05 13:33:06 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# get script directory, i.e. dtk-arbiter root
+base_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# make sure puppet-omnibus is first in path
+export PATH=/opt/puppet-omnibus/embedded/bin/:${PATH}
+
+cd ${base_dir}
+# reset everything just in case and then pull
+git reset --hard
+git pull
+
+bundle install
