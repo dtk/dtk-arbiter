c679d40551387920341029d8b5da1d66ec2c8426
Haris Krajina <haris@atlantbh.com>
2015-12-10 14:41:10 +0100

    Add changes to puppet runner

--- lib/utils/puppet_runner.rb
+++ lib/utils/puppet_runner.rb
@@ -1,12 +1,24 @@
 require 'puppet'
 
+class Puppet::Settings
+  def initialize_global_settings(args = [])
+    #raise Puppet::DevError, "Attempting to initialize global default settings more than once!" if global_defaults_initialized?
+    return if global_defaults_initialized?
+    # The first two phases of the lifecycle of a puppet application are:
+    # 1) Parse the command line options and handle any of them that are
+    #    registered, defined "global" puppet settings (mostly from defaults.rb).
+    # 2) Parse the puppet config file(s).
+    parse_global_options(args)
+    parse_config_files
+    @global_defaults_initialized = true
+  end
+end
+
 module Arbiter
   module Utils
     class PuppetRunner
 
       def self.apply(puppet_definition, resource_hash)
-        # DEBUG SNIPPET >>> REMOVE <<<
-        require (RUBY_VERSION.match(/1\.8\..*/) ? 'ruby-debug' : 'debugger');Debugger.start; debugger
         if Puppet.settings.respond_to?(:initialize_global_settings)
           Puppet.settings.initialize_global_settings
         end
@@ -14,6 +26,10 @@ module Arbiter
         if Puppet.settings.respond_to?(:initialize_app_defaults)
           Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
         end
+
+        Puppet.settings.initialize_global_settings
+        Puppet.settings.initialize_app_defaults(Puppet::Settings.app_defaults_for_run_mode(Puppet.run_mode))
+
 
         pup = Puppet::Type.type(:ssh_authorized_key).new(resource_hash)
         catalog = Puppet::Resource::Catalog.new
