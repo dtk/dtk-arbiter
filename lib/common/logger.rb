70b61d3f07156c27bd0f492a0e69d81d049e8363
Haris Krajina <haris@atlantbh.com>
2015-12-18 12:17:10 +0100

    Timestamp, task id, agent, action name are now forming naming pattern for action logs

--- lib/common/logger.rb
+++ lib/common/logger.rb
@@ -52,7 +52,7 @@ module Arbiter
     end
 
     def self.log_results(params_in, results, agent_name, action_name, top_task_id, task_id, worker_name)
-      component_dir = File.join(LOGS_DIR, "#{top_task_id}_#{agent_name}##{action_name}")
+      component_dir = File.join(LOGS_DIR, "#{Time.now.to_i}_#{task_id}_#{agent_name}##{action_name}")
       FileUtils.mkdir_p(component_dir, mode: 0755) unless File.directory?(component_dir)
 
       filename = File.join(component_dir, "#{task_id}_#{action_name}.log")
