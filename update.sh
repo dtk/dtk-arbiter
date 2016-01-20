94c5b2845ad2fdacb6f2afa1ffbc447d001a5018
dduvnjak <dario.duvnjak@gmail.com>
2016-01-20 13:04:26 +0100

    DTK-2409: Make update script configurable with branch

--- update.sh
+++ update.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+## usage:
+#  ./update.sh [branch]
+
 # get script directory, i.e. dtk-arbiter root
 base_dir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # make sure puppet-omnibus is first in path
 export PATH=/opt/puppet-omnibus/embedded/bin/:${PATH}
+# default branch to stable
+branch=${1:-stable}
 
 cd ${base_dir}
-# reset everything just in case and then pull
-git fetch
-git reset --hard origin/stable
-
-bundle install
+if git rev-parse --verify origin/${branch}; then
+  # reset everything just in case and then pull
+  git fetch
+  git reset --hard origin/${branch}
+  bundle install
+else
+  echo "Branch ${branch} doesn't seem to exist. Aborting upgrade"
+  exit 1
+fi
