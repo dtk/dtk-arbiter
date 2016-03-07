1add882e35bf18889ac0ff71b61bc606f3ab42c4
Haris Krajina <haris@atlantbh.com>
2016-03-07 15:41:33 +0100

    Make sure that these changes do not fail due to nil result

--- lib/common/mixin/open3.rb
+++ lib/common/mixin/open3.rb
@@ -92,8 +92,8 @@ module Arbiter
           err_r.close unless err_r.closed?
         end
 
-        result[:stdout] = result[:stdout].encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "?")
-        result[:stderr] = result[:stderr].encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "?")
+        result[:stdout] = result[:stdout].encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "?") if result[:stdout]
+        result[:stderr] = result[:stderr].encode("UTF-8", :invalid => :replace, :undef => :replace, :replace => "?") if result[:stderr]
 
         [result[:stdout], result[:stderr], result[:status], result]
       end
