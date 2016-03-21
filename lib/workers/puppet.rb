3e2b73b55083e03cf11566266dcfbb9427d9fbc0
Haris Krajina <haris@atlantbh.com>
2016-03-21 13:04:49 +0100

    Minor changes to wait time

--- lib/workers/puppet.rb
+++ lib/workers/puppet.rb
@@ -143,7 +143,7 @@ module Arbiter
             end
             Log.info("Puppet execution is resuming operation since YUM process has finished! Waiting another #{YUM_WAIT_PERIOD} to check if another YUM process starts")
             # this is temp solution to check if another yum process
-            sleep 120
+            sleep 150
             wait_for_yum_lock_release
           end
         end
