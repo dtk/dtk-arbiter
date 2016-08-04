35ac4b21632e4499c01fb7e449602265ea43c572
dduvnjak <dario@atlantbh.com>
2016-08-04 14:05:47 +0000

    Support json dynamic attributes

--- lib/docker/commander.rb
+++ lib/docker/commander.rb
@@ -25,8 +25,7 @@ module Arbiter
       end
 
       def run
-        # require 'debugger'; debugger
-        docker_container_name = "dtk#{Time.now.to_i}"
+        docker_container_name = @docker_run_params[:name] || "dtk#{Time.now.to_i}"
         output_dir = "/usr/share/dtk/docker-worker/#{docker_container_name}"
         output_dir_tmp = "#{output_dir}/tmp"
         output_dir_container = "/host_volume"
@@ -34,8 +33,8 @@ module Arbiter
         puppet_modules_dir = '/usr/share/dtk/puppet-modules'
         ## get the module that invoked the docke action
         # remove namespace
-        module_name_short = @module_name.split(':')[1]
-        module_absolute_location = "#{puppet_modules_dir}/#{module_name_short}"
+        @module_name_short = @module_name.split(':')[1]
+        module_absolute_location = "#{puppet_modules_dir}/#{@module_name_short}"
 
         FileUtils.mkdir_p output_dir_tmp
         # make sure dtkyaml reporter is available to puppet
@@ -49,20 +48,23 @@ module Arbiter
         if @dockerfile
           # generate the dockerfile in the module root
           # use a name with a timestamp to avoid overriting any possible existing Dockerfiles
-          dockerfile_name = "Dockerfile.#{docker_container_name}"
+          dockerfile_name = "Dockerfile.dtk#{Time.now.to_i}"
           dockerfile_location = "#{module_absolute_location}/#{dockerfile_name}"
+          Log.info("Generating Dockerfile: #{dockerfile_location}")
           File.open(dockerfile_location, 'w') { |file| file.write(@dockerfile) }
           Log.info "Building docker image: #{@docker_image_tag}"
           # image = ::Docker::Image.build(@dockerfile)
           image = ::Docker::Image.build_from_dir(module_absolute_location, { 'dockerfile' => dockerfile_name })
           image.tag('repo' => @docker_image_tag, 'force' => true)
           # delete the temporary Dockerfile
-          FileUtils.rm(dockerfile_location)
+          #FileUtils.rm(dockerfile_location)
         end
 
         docker_cli_cmd = "docker run --name #{docker_container_name} -v #{output_dir}:#{output_dir_container} -v #{output_dir}/tmp:/tmp" +
-                        ((@execution_type.eql? 'puppet') ? " -v #{puppet_modules_dir}:/etc/puppet/modules" : "") + " #{@docker_image_final} #{@docker_command}"
-
+                        ((@execution_type.eql? 'puppet') ? " -v #{puppet_modules_dir}:/etc/puppet/modules" : "") + 
+                        " -v #{puppet_modules_dir}:#{puppet_modules_dir}" + 
+                        get_cli_args(@docker_run_params, module_absolute_location) +
+                        " #{@docker_image_final} #{@docker_command}"
         Log.info "Starting Docker container..."
 
         docker_run_stdout, docker_run_stderr, exit_status, results = capture3_with_timeout(docker_cli_cmd)
@@ -76,7 +78,7 @@ module Arbiter
           end
         end
 
-        container = ::Docker::Container.get(docker_container_name)
+        container = ::Docker::Container.get(docker_container_name) unless @docker_run_params[:rm]
 
         @results = Hash.new
 
@@ -84,11 +86,11 @@ module Arbiter
         @results[:stdout] = docker_run_stdout
         @results[:stderr] = docker_run_stderr
         @results[:status] = exit_status.exitstatus
-        @results[:dynamic_attributes] = read_dynamic_attributes(output_dir_tmp)
+        @results[:dynamic_attributes] = read_dynamic_attributes(output_dir_tmp) || read_dynamic_attributes(output_dir_tmp, 'dtk_exported_variables_json', 'json')
 
         # cleanup
-        Log.info("Deleting container and doing cleanup")
-        container.delete(:force => true)
+#        Log.info("Deleting container and doing cleanup")
+#        container.delete(:force => true)
 
         # http://unix.stackexchange.com/a/117848
         system("find #{output_dir} -depth -type f -exec shred -n 1 -z -u {} \\;")
@@ -101,28 +103,36 @@ module Arbiter
 
     private
 
-      def get_cli_args (msg, module_location)
-        cli_args = ''
+      def get_cli_args (docker_run_params, module_location)
+        cli_args = ' '
         # add env vars
-        cli_args += msg[:docker_run_params][:environment].map{|k,v| "-e #{k}=#{v}"}.join(' ') + ' ' if msg[:docker_run_params][:environment]
+        cli_args += docker_run_params[:environment].map{|k,v| "-e #{k}=#{v}"}.join(' ') + ' ' if docker_run_params[:environment]
         # add entrypoint
-        cli_args += "--entrypoint #{module_location}/#{msg[:docker_run_params][:entrypoint]} "
+        cli_args += " --entrypoint #{module_location}/#{docker_run_params[:entrypoint]} "
         # add volumes
-        cli_args += msg[:docker_run_params][:volumes].map{|a| "-v #{module_location}/#{a}"}.join(' ') + ' ' if msg[:docker_run_params][:volumes]
+        cli_args += docker_run_params[:volumes].map{|a| "-v #{module_location}/#{a}"}.join(' ') + ' ' if docker_run_params[:volumes]
         # set privileged
-        cli_args += '--privileged ' if msg[:docker_run_params][:privileged]
+        cli_args += ' --privileged ' if docker_run_params[:privileged]
         # set rm
-        cli_args += '--rm ' if msg[:docker_run_params][:rm]
+        cli_args += ' --rm ' if docker_run_params[:rm]
         # set name
-        cli_args += "--name #{msg[:docker_run_params][:name]} " if msg[:docker_run_params][:name]
+        # cli_args += "--name #{docker_run_params[:name]} " if docker_run_params[:name]
 
         cli_args
       end
 
-      def read_dynamic_attributes(path, identifier = 'dtk_exported_variables')
+      def read_dynamic_attributes(path, identifier = 'dtk_exported_variables', format = 'marshal')
         full_path = File.join(path, identifier)
         return nil unless File.exists?(full_path)
-        Marshal.load(File.read(full_path))
+        case format
+        when 'marshal'
+          Marshal.load(File.read(full_path))
+        when 'json'
+          json_dump = JSON.parse(File.read(full_path))
+          @module_name_short.nil? ? json_dump : {@module_name_short => json_dump}
+        else
+          return nil
+        end
       end
 
     end
