da9c2fb44ae44084102601f4cf9e872086f597ed
dduvnjak <dario.duvnjak@gmail.com>
2017-08-28 10:35:49 +0200

    DTK-3114: Disable SSH key sanitization due to bug

--- lib/arbiter/utils/sanitize.rb
+++ lib/arbiter/utils/sanitize.rb
@@ -4,7 +4,6 @@ module DTK::Arbiter
       HIDDEN_VALUE = '***'
 
       def self.sanitize_message(msg)
-        msg[:agent_ssh_key_private] = HIDDEN_VALUE if msg.key?(:agent_ssh_key_private)
         return msg unless msg[:protocol_version]
         sanitized_attributes = msg[:attributes].inject({}) do |h, (type, attributes_hash)| 
           h.merge(type => attributes_hash.inject({}) { |h, (name, info)| h.merge(name => sanitize_attribute(name, info)) })
