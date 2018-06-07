8a1c7a5629c6ed908e3f96ee6ca44b57241f57a3
dduvnjak <dario.duvnjak@gmail.com>
2018-06-07 15:46:26 +0200

    Increase docker timeouts when building

--- lib/arbiter/worker/generic/docker/image.rb
+++ lib/arbiter/worker/generic/docker/image.rb
@@ -1,7 +1,12 @@
 module DTK::Arbiter
   module Worker::Generic::Docker
     module Image
+
+      DOCKER_TIMEOUT = 300
+
       def self.build(dockerfile, docker_image_tag)
+        Docker.options[:read_timeout] = DOCKER_TIMEOUT
+        Docker.options[:write_timeout] = DOCKER_TIMEOUT
         docker_image = ::Docker::Image.build(dockerfile)
         docker_image.tag('repo' => docker_image_tag, 'force' => true)
         docker_image
